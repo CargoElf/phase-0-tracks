e9411a1831d7169fc2806f6ee5f16e1bc91dd9fd
Ben Van Sickle <benjamin.m.vansickle@gmail.com>
2016-05-04 14:17:56 -0500

    Add pseudo code for encrypt and decrypt

---
+++ ruby/secret_agents.rb
@@ -0,0 +1,21 @@
+
+=begin
+- Ask for input
+- Get input
+  - Loop through each letter of input until end of string
+   - If not a space
+     - Shift each letter to the next sequential letter
+  - Else leave space unchanged
+=end
+
+=begin
+# - Get input
+# - Loop through each letter of input until end of string
+#   - If not a space
+#     - Convert letter to ASCII value
+#     - Subtract 1 from that value
+#     - Convert ASCII value to character
+#   - Else leave space unchanged
+=end
+
+
