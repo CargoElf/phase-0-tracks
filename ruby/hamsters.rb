8f4c051b33d407ac1b1f6045df92e84eb549fc2f
Ben Van Sickle <benjamin.m.vansickle@gmail.com>
2016-05-03 23:16:10 -0500

    Add until loop to make sure age var is in a realistic range

--- ruby/hamsters.rb
+++ ruby/hamsters.rb
@@ -35,11 +35,17 @@ end
 puts "About how old is the hamster in years? Leave blank if not sure."
 age = gets.chomp
 
+until age.to_i>0 && age.to_i<20
+  puts "Please enter an age that makes sense! Preferably one that is a number, like 4."
+  age = gets.chomp
+end
+
 if age == ""
   age = nil
 end
 
 vol_lvl = vol_lvl.to_i
+
 if age != nil
   age = age.to_i
 end
