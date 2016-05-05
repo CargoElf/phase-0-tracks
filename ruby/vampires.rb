9be041484b9a2fdad3c39d760841ad956eb181db
Ben Van Sickle <benjamin.m.vansickle@gmail.com>
2016-05-05 15:48:40 -0500

    Add second conditions, age wrong doesn't like grlic bread or doesn't like insurance

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -2,10 +2,10 @@ puts "What is your name?"
 name = gets.chomp
 
 puts "How old are you?"
-age = gets.chomp
+age = gets.chomp.to_i
 
 puts"What year year were you born?"
-year_ob = gets.chomp
+year_ob = gets.chomp.to_i
 
 puts "Our company cafeteria serves garlic bread. Should we order some for you? (y/n)"
 garlic_bread = gets.chomp
@@ -13,7 +13,8 @@ garlic_bread = gets.chomp
 puts "Would you like to enroll in the company's health insurance? (y/n)"
 insuracne = gets.chomp[0].downcase
 
-
-if (year_ob.to_i + age.to_i <= Time.new.year - 1) && (garlic_bread == "y" || insuracne == "y")
-  puts "Probably not a vampire"
+if (year_ob + age <= Time.new.year - 1) && (garlic_bread == "y" || insuracne == "y")
+  puts "Probably not a vampire."
+elsif (year_ob + age > Time.new.year - 1) && (garlic_bread != "y" || insurance != "y")
+  puts "Probably a vampire."
 end
