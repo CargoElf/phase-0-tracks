2a837379b7f8a2f2c5dcec6b42925ab47987f1fa
Ben Van Sickle <benjamin.m.vansickle@gmail.com>
2016-05-05 15:36:30 -0500

    Add first conditions, age correct and  garlic bread or insurance

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -7,8 +7,14 @@ age = gets.chomp
 puts"What year year were you born?"
 year_ob = gets.chomp
 
-puts "Our company cafeteria serves garlic bread. Should we order some for you?"
+puts "Our company cafeteria serves garlic bread. Should we order some for you? (y/n)"
 garlic_bread = gets.chomp
 
 puts "Would you like to enroll in the company's health insurance? (y/n)"
 insuracne = gets.chomp[0].downcase
+
+
+
+if (year_ob.to_i + age.to_i == Time.new.year - 1 || Time.new.year) && (garlic_bread == "y" || insuracne == "y")
+  puts "Probably not a vampire"
+end
