92eaf27f262f6e610deb4b82fc8797a4075d8ee2
Ben Van Sickle <benjamin.m.vansickle@gmail.com>
2016-05-05 15:02:24 -0500

    Add input vars for vamp detection

---
+++ ruby/vampires.rb
@@ -0,0 +1,14 @@
+puts "What is your name?"
+name = gets.chomp
+
+puts "How old are you?"
+age = gets.chomp
+
+puts"What year year were you born?"
+year_ob = gets.chomp
+
+puts "Our company cafeteria serves garlic bread. Should we order some for you?"
+garlic_bread = gets.chomp
+
+puts "Would you like to enroll in the company's health insurance? (y/n)"
+insuracne = gets.chomp[0].downcase
