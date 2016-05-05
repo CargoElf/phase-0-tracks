aeec7c2b03a3bc9da91d9ee15aeb0ec9f7d2671a
Ben Van Sickle <benjamin.m.vansickle@gmail.com>
2016-05-05 16:45:36 -0500

    Changed order of if statment and cleaned up first if statement.

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,5 +1,8 @@
 puts "What is your name?"
 name = gets.chomp
+valid_input(name)
+
+puts name
 
 puts "How old are you?"
 age = gets.chomp.to_i
@@ -8,12 +11,14 @@ puts"What year year were you born?"
 year_ob = gets.chomp.to_i
 
 puts "Our company cafeteria serves garlic bread. Should we order some for you? (y/n)"
-garlic_bread = gets.chomp
+garlic_bread = gets.chomp[0].downcase
 
 puts "Would you like to enroll in the company's health insurance? (y/n)"
 insurance = gets.chomp[0].downcase
 
-if (year_ob + age >= Time.new.year - 1) && (garlic_bread == "y" || insurance == "y")
+if name == "Drake Cula" || name == "Tu Fang")
+  puts "Definitely a vampire."
+elsif (year_ob + age >= Time.new.year - 1) && (garlic_bread == "y" || insurance == "y") 
   puts "Probably not a vampire."
 elsif (year_ob + age < Time.new.year - 1) && (garlic_bread != "y" || insurance != "y")
   puts "Probably a vampire." unless (year_ob + age < Time.new.year - 1) && (garlic_bread != "y" && insurance != "y")
