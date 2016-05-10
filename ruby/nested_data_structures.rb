6c8fdefaf661184317d4abdd90767e7f8c2c4968
Ben Van Sickle <benjamin.m.vansickle@gmail.com>
2016-05-10 13:34:35 -0500

    Add in some blank hashes to fill in later

--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -1,7 +1,40 @@
 highway = {
   red_car: {
     car_type: "Toyota",
+    car_size: "Mid Size",
     num_doors: 4,
+    passengers: [ "Tom", "Jerry", "Julie", "Jane"
+    ],
+    cargo: [ "Apples", "Beer", "Boccie Ball"
+    ]
+  }
+  semi_truck: {
+    driver: "Big Jim",
+    cargo: [ "Slim Jims", "Sparkling Wine", "Cheez-its"
+    ]
+  }
+  __car: {
+    car_type: ,
+    car_size: 
+    num_doors: ,
+    passengers: [
+    ],
+    cargo: [
+    ]
+  }
+  __car: {
+    car_type: ,
+    car_size: 
+    num_doors: ,
+    passengers: [
+    ],
+    cargo: [
+    ]
+  }
+  __car: {
+    car_type: ,
+    car_size: 
+    num_doors: ,
     passengers: [
     ],
     cargo: [
