439b9b1587d1164997be9adad5bd4b386ae09d4c
Ben Van Sickle <benjamin.m.vansickle@gmail.com>
2016-05-09 21:19:54 -0500

    Add pseudo code

---
+++ ruby/hashes.rb
@@ -0,0 +1,10 @@
+=begin
+***pseudo code***
+For each key in hash, prompt user for input to assign to value.
+  Assign value to appropriate key
+  Change each value to the appropriate data type.
+Display the hash after all values have been assigned.
+Prompt user for any changes needed any one key value.
+  Get key name
+  Get new value and update hash
+Display the hash.
