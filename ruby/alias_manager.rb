e1df3082bbd877f74500f45154818760c7aab6a0
Ben Van Sickle <benjamin.m.vansickle@gmail.com>
2016-05-11 20:30:53 -0500

    Add pseudo code and method for swapping first and last name

---
+++ ruby/alias_manager.rb
@@ -0,0 +1,20 @@
+=begin
+Take in agent name
+Spit name into array
+  Switch first and last name
+Check each letter of array
+  for each vowel, increment to the next vowel
+  for each consonant, increment to the next consonant
+=end
+
+#Switches first and last name. Turns works with multi-word
+#last names.
+def name_swap(name)
+  full_name = name.split
+  first_name = full_name.shift
+  last_name = full_name.join(' ')
+  full_name = "#{last_name} #{first_name}"
+end
+
+agent_name = 'Ben Van Sickle'
+puts name_swap(agent_name)
