1cf9ae4bf57e7cb1571d23e4555d0a5e47c97cf5
Ben Van Sickle <benjamin.m.vansickle@gmail.com>
2016-05-11 21:08:06 -0500

    Add consonant and vowel hashes

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -10,11 +10,19 @@ Check each letter of array
 #Switches first and last name. Turns works with multi-word
 #last names.
 def name_swap(name)
-  full_name = name.split
+  puts full_name = name.split(" ")
   first_name = full_name.shift
-  last_name = full_name.join(' ')
+  last_name = full_name.join(" ")
   full_name = "#{last_name} #{first_name}"
 end
 
-agent_name = 'Ben Van Sickle'
+def scramble(name)
+  vowels = {0=>"a" ,1=>"e", 2="i", 3=>"o", 4=>"u"}
+  consonants = {0: "b", 1: "c". 2: "d", 3: "f", 4: "g",
+    5: "h", 6: "j", 7: "k", 8: "l", 9: "m", 10: "n", 11: "p",
+    12: "q", 13: "r", 14: "s", 15: "t", 16: "v", 17: "w",
+    18: "x", 19: "y", 20: "z"}
+  name.map! {|x| if x =
+
+agent_name = "Ben Van Sickle"
 puts name_swap(agent_name)
