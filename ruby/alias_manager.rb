aa8cb4a4204aeafd1e13caaf25119ea59b140980
Ben Van Sickle <benjamin.m.vansickle@gmail.com>
2016-05-12 19:25:44 -0500

    Simplify arrays to strings

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -16,13 +16,11 @@ def name_swap(name)
   full_name = "#{last_name} #{first_name}"
 end
 
-def scramble(name)
-  vowels = {0=>"a" ,1=>"e", 2="i", 3=>"o", 4=>"u"}
-  consonants = {0: "b", 1: "c". 2: "d", 3: "f", 4: "g",
-    5: "h", 6: "j", 7: "k", 8: "l", 9: "m", 10: "n", 11: "p",
-    12: "q", 13: "r", 14: "s", 15: "t", 16: "v", 17: "w",
-    18: "x", 19: "y", 20: "z"}
-  name.map! {|x| if x =
+def vowel_incr(name)
+  vowels = "aeiou"
+
+def consonant_incr(name)  
+  consonants = "bcdfghjklmnpqrstvwxyz"
 
 agent_name = "Ben Van Sickle"
 puts name_swap(agent_name)
