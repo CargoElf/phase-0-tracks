a01eabad15674a5390eea248131a99362f205074
Ben Van Sickle <benjamin.m.vansickle@gmail.com>
2016-05-14 20:15:04 -0500

    Tighten up name_swap code, add comments

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -1,54 +1,40 @@
 =begin
 Take in agent name
 Spit name into array
-  Switch first and last name
-Check each letter of array
-  for each vowel, increment to the next vowel
-  for each consonant, increment to the next consonant
+  Recombine switching first and last name
+Check each letter of letter of string
+  for each consonant, replace with the next consonant
+    for "z" replace with "b"
+  for each vowel, replace with the next vowel
+    for "u" replace with "a"
 =end
 
-#Switches first and last name. Turns works with multi-word
-#last names.
+# Switches first and last name. Turns works with multi-word
+# last names. splits name at spaces into array, bumps them 
+# one and turns back into string.
 def name_swap(name)
-  full_name = name.split(" ") #splits name at spaces into array
-  first_name = full_name.shift 
-  last_name = full_name.join(" ")
-  full_name = "#{last_name} #{first_name}"
+  full_name = name.split.rotate.join(" ") 
 end
 
+# takes in name and first transforms consonants, then vowels
 def name_scram(name)
   name = name.downcase.tr("bcdfghjklmnpqrstvwxyz", "cdfghjklmnpqrstvwxyzb")
   name = name.downcase.tr("aeiou", "eioua")
 end
 
-=begin
-def name_scram(name)
-  name.each_char do |i|
-    if name[i] == "z"
-      name[i] = "a"
-    elsif name[i] != " "
-      name[i] = name[i].next
-    end
-  end
-end
-=end
-
 aliases = []
-
-#agent_name = "Ben Van Sickle"
-#puts name_scram(name_swap(agent_name)).split.map(&:capitalize).join(' ')
-
 choice = ""
-while choice != "quit"
+while choice != "quit" # runs until quit is entered after agent name
   puts "Enter a name:"
   agent_name = gets.chomp
   code_name = name_scram(name_swap(agent_name.downcase)).split.map(&:capitalize).join(' ')
-  puts code_name
+  puts "Your code name is #{code_name}!"
   aliases << [agent_name, code_name]
   puts "Press the 'enter' key to input another name or type 'quit' to stop"
   choice = gets.chomp
 end
 
+# iterates through array and prints real name + code name
 aliases.each do |name, code|
-  puts "Real Name:#{name}, Code Name: #{code}"
+  puts "Real Name: #{name}, Code Name: #{code}"
 end
