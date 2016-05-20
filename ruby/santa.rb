9d5c7871c2e569257cd6a63c9819fb049a282f8c
Ben Van Sickle <benjamin.m.vansickle@gmail.com>
2016-05-19 19:27:09 -0500

    Finish release 3

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,11 +1,11 @@
 class Santa
 
-  def initialize (gender, ethnicity)
+  def initialize (gender, ethnicity, age)
     puts "Initializing Santa instance..."
     @gender = gender
     @ethnicity = ethnicity
     @reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
-    @age = 0
+    @age = age
   end
 
   def speak
@@ -29,6 +29,10 @@ class Santa
     @gender = new_gender
   end
 
+  def age=(new_age)
+    @age = new_age
+  end
+
   def gender
     @gender
   end
@@ -37,19 +41,36 @@ class Santa
     @age
   end
 
+  def ethnicity=(new_ethnicity)
+    @ethnicity = new_ethnicity
+  end
+
   def ethnicity
     @ethnicity
   end
 
 end
 
-santas = []
+#santas = []
 example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
 example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
-example_genders.length.times do |i|
-  santas << Santa.new(example_genders[i], example_ethnicities[i])
+#example_genders.length.times do |i|
+#  santas << Santa.new(example_genders[i], example_ethnicities[i])
+#end
+
+
+
+10000.times do |i|
+  count = i + 1
+  i = Santa.new(example_genders.sample,example_ethnicities.sample,rand(140))
+  puts "Santa #{count}'s gender is #{i.gender}, ethnicity is #{i.ethnicity} and is #{i.age} in years."
 end
 
+
+
+
+
+=begin
 test = Santa.new("Female","Hispanic")
 puts test.get_mad_at("Vixen")
 test.celebrate_birthday
@@ -57,3 +78,4 @@ puts "Happy birthday! You're #{test.age} now!"
 puts "Ethnicity is #{test.ethnicity}."
 test.gender = "gender fluid"
 puts "Gender is #{test.gender}"
+=end
