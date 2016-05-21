43ef6f44c3354bfd728980629ddaf751452bb75c
Ben Van Sickle <benjamin.m.vansickle@gmail.com>
2016-05-21 10:57:10 -0500

    Add driver code

--- ruby/solo.rb
+++ ruby/solo.rb
@@ -1,28 +1,36 @@
 class Warrior
 
   attr_reader :strength, :weapon
-  attr_accessor :attack, :hit_poits
+  attr_accessor :enemy, :hit_points
 
   def initialize (hit_points, strength, weapon)
     puts "A new warrior enters the world!"
-    @hit_point = hit_point
+    @hit_points = hit_points
     @strength = strength
     @weapon = weapon
   end
 
   def take_damage(dmg_amount)
-    @hit_point = hit_point - dmg_amount
+    @hit_points = hit_points - dmg_amount
   end
 
   def attack(enemy)
-    @attack = enemy
+    @enemy = enemy
   end
 
   def heal(healing)
-    @hit_point = hit_point + healing
+    @hit_points = hit_points + healing
   end
 
 end
 
 stan = Warrior.new(23, 14, "sword")
-puts stan.take_damge(3)
+puts "Warrior's strength is #{stan.strength}!"
+stan.take_damage(3)
+puts "Current hit points: #{stan.hit_points}"
+stan.heal(2)
+puts "Current hit points: #{stan.hit_points}"
+stan.attack("goblin")
+puts "Warrior attacks #{stan.enemy} with #{stan.weapon}!"
+
+
