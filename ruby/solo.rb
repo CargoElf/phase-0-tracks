691fe14e33c8aa5237519b98bfa2a8ef8c0b46b0
Ben Van Sickle <benjamin.m.vansickle@gmail.com>
2016-05-21 10:35:44 -0500

    Add initialize attributes

---
+++ ruby/solo.rb
@@ -0,0 +1,10 @@
+class Warrior
+
+  def initialize (hit_point, strength, weapon)
+    puts "A new warrior enters the world!"
+    @hit_point = hit_point
+    @strength = strength
+    @weapon = weapon
+  end
+
+end
