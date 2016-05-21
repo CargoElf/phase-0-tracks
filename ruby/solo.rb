b906e19aad004d5b01717ee34fa44846a9bc35d9
Ben Van Sickle <benjamin.m.vansickle@gmail.com>
2016-05-21 11:31:23 -0500

    Add generating loop

--- ruby/solo.rb
+++ ruby/solo.rb
@@ -39,6 +39,31 @@ class Warrior
 
 end
 
+warriors = []
+puts "We're storming the castle! I'm going to magic up some warriors!"
+
+count = 0
+choice = nil
+
+until choice == "stop"
+  i = count 
+  puts "What shall we call this warrior, my liege?"
+  name = gets.chomp
+  puts "How strong shall this warrior be? (enter a number)"
+  strength = gets.chomp.to_i
+  puts "How many hit points should I give to the warrior? (enter a number)"
+  hit_points = gets.chomp.to_i
+  puts "What weapon should we arm them with?"
+  weapon = gets.chomp
+  warriors << Warrior.new(hit_points, strength, weapon, name)
+  count += 1
+  puts "I'm going to make another, unless you tell me to stop! (enter 'stop' to stop)"
+  choice = gets.chomp
+end
+
+puts warriors
+
+=begin
 stan = Warrior.new(23, 14, "sword", "Edwardo")
 puts "#{stan.name}'s' strength is #{stan.strength}!"
 stan.take_damage(3)
@@ -47,5 +72,4 @@ stan.heal(2)
 puts "Current hit points: #{stan.hit_points}"
 stan.attack("goblin")
 puts "#{stan.name} attacks #{stan.enemy} with #{stan.weapon}!"
-
-
+=end
