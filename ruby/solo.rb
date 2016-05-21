1d7d2e2ddf30367eae523c900cce346096377c96
Ben Van Sickle <benjamin.m.vansickle@gmail.com>
2016-05-21 11:04:56 -0500

    Add pseudocode and update driver code

--- ruby/solo.rb
+++ ruby/solo.rb
@@ -1,13 +1,28 @@
+=begin
+Warrior
+
+On initialize, take hit points, strength, weapon, and name
+When damage is taken
+  subtract damage amount from current hit points
+  update hit points
+When healing
+  add healing amount to current hit points
+  update hit points
+When attacking
+  store enemy name for retrieval later
+=end
+
 class Warrior
 
-  attr_reader :strength, :weapon
+  attr_reader :strength, :weapon, :name
   attr_accessor :enemy, :hit_points
 
-  def initialize (hit_points, strength, weapon)
+  def initialize (hit_points, strength, weapon, name)
     puts "A new warrior enters the world!"
     @hit_points = hit_points
     @strength = strength
     @weapon = weapon
+    @name = name
   end
 
   def take_damage(dmg_amount)
@@ -24,13 +39,13 @@ class Warrior
 
 end
 
-stan = Warrior.new(23, 14, "sword")
-puts "Warrior's strength is #{stan.strength}!"
+stan = Warrior.new(23, 14, "sword", "Edwardo")
+puts "#{stan.name}'s' strength is #{stan.strength}!"
 stan.take_damage(3)
 puts "Current hit points: #{stan.hit_points}"
 stan.heal(2)
 puts "Current hit points: #{stan.hit_points}"
 stan.attack("goblin")
-puts "Warrior attacks #{stan.enemy} with #{stan.weapon}!"
+puts "#{stan.name} attacks #{stan.enemy} with #{stan.weapon}!"
 
 
