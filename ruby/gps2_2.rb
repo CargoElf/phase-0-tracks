414de6f0075fa1fa9bcbee20ddd7f091f9f5fddc
Ben Van Sickle <benjamin.m.vansickle@gmail.com>
2016-05-21 18:54:48 -0500

    Add pseudocode

--- ruby/gps2_2.rb
+++ ruby/gps2_2.rb
@@ -1,29 +1,44 @@
 =begin
+
 Method to create a list
 input: string of items separated by spaces (example: "carrots apples cereal pizza")
 steps: 
-[fill in any steps here]
-  set default quantity
-  print the list to the console [can you use one of your other methods here?]
-  output: [what data type goes here, array or hash?]
+  create empty hash and set default quantity to one
+  take user input and split into array
+  use array blocks for hash keys
+  print the list to the console using print method
+output: hash
 
 Method to add an item to a list
   input: item name and optional quantity
   steps:
-  output:
+    get user input for item
+    get user input for quantity
+    use item for key and quantity for value
+    store in hash
+  output: updated hash
 
 Method to remove an item from the list
-  input:
+  input: item name
   steps:
-  output:
+    get user input for item to be removed
+    remove item from hash
+  output: updated hash
 
 Method to update the quantity of an item
-  input:
+  input: item name and quantity
   steps:
-  output:
+    get user input for item
+    get user input for new quantity
+    update quantity in hash
+  output: updated hash
 
 Method to print a list and make it look pretty
-  input:
+  input: hash
   steps:
-  output:
+    print "Your cart contains"
+    iterate through hash
+      print quantity of item then item name
+  output: informational formated list
+
 =end
