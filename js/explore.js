a83bbef4890fa3639c3ea1b80acd28cfeb13f94a
Ben Van Sickle <benjamin.m.vansickle@gmail.com>
2016-05-25 19:25:47 -0500

    Put in pseudocode

---
+++ js/explore.js
@@ -0,0 +1,10 @@
+/*
+String reverse method
+input: string
+For each letter starting with the last
+  Add to new variable to rebuild word backwards.
+output: string reversed
+*/
+
+var wordReverse = ""
+function reverse(word)
