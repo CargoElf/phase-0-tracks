9059cc9ddf51e2bc83ecf972039ad9c5e7128d57
Ben Van Sickle <benjamin.m.vansickle@gmail.com>
2016-05-25 19:54:53 -0500

    Add palindrome detection

--- js/explore.js
+++ js/explore.js
@@ -8,14 +8,17 @@ output: string reversed
 
 function reverse(word) {
   var wordReverse = '';
-  for (var i = word.length - 1; i >= 0; i--)
+  for (var i = word.length - 1; i >= 0; i--) //start at end of string and work down
     wordReverse += word[i];
   return wordReverse;
 }
 
-var reverseResult =  reverse("InputWord");
+var inputWord = "racecar";
+var reverseResult =  reverse(inputWord);
 
-if (reverseResult.length  <= 8) {
+if (reverseResult == inputWord) { //palindrome detection!
+  console.log( inputWord + " is a palindrome!");
+} else if (reverseResult.length  <= 8) {
   console.log(reverseResult);
 } else {
   console.log("Why did you pick a long word like " + reverse(reverseResult) + "?");
