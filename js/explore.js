b0a8ae14b3c3d52dfcf00c8bf57f2891535abb17
Ben Van Sickle <benjamin.m.vansickle@gmail.com>
2016-05-25 19:38:44 -0500

    Add function and driver code.

--- js/explore.js
+++ js/explore.js
@@ -6,5 +6,12 @@ For each letter starting with the last
 output: string reversed
 */
 
-var wordReverse = ""
-function reverse(word)
+function reverse(word) {
+  var wordReverse = '';
+  for (var i = word.length - 1; i >= 0; i--)
+    wordReverse += word[i];
+  return wordReverse;
+}
+
+var reverseResult =  reverse("InputWord");
+console.log(reverseResult);
