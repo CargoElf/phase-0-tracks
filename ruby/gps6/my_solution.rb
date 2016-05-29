d47d997ac49a17c5e69b248ad0654bf4acb938d2
Ben Van Sickle <benjamin.m.vansickle@gmail.com>
2016-05-29 17:32:51 -0500

    Refactor speed of spread to formula

--- ruby/gps6/my_solution.rb
+++ ruby/gps6/my_solution.rb
@@ -47,15 +47,15 @@ class VirusPredictor
   def speed_of_spread #in months
   # We are still perfecting our formula here. The speed is also affected
   # by additional factors we haven't added into this functionality.
-  speed = 2.5
+    speed = 2.5
 
-  if @population_density >= 200
-    speed = 0.5
-  elsif @population_density >= 50 && @population_density < 200
-    speed = speed - ((@population_density / 50).floor / 2.0)
-  end
+    if @population_density >= 200
+      speed = 0.5
+    elsif @population_density >= 50 && @population_density < 200
+      speed = speed - ((@population_density / 50).floor / 2.0)
+    end
 
-  puts " and will spread across the state in #{speed} months.\n\n"
+    puts " and will spread across the state in #{speed} months.\n\n"
 
   end
 
