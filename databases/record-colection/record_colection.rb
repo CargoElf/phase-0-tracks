bc560eb23c3f6690781dcf6f58c49694fe078348
Ben Van Sickle <benjamin.m.vansickle@gmail.com>
2016-06-06 06:44:12 -0500

    Add interface

--- databases/record-colection/record_colection.rb
+++ databases/record-colection/record_colection.rb
@@ -31,7 +31,14 @@ require_relative 'db_setup'
 db = SQLite3::Database.new("record_colection.db")
 db.results_as_hash = true
 
-def artist_entry(db, artist_name)
+#def artist_entry_exists(db, entry) #need to explore more.
+#  if db.execute("SELECT a.artist FROM artists a WHERE a.artist = #{entry}")
+#  end
+#end
+
+#Checks if entry exists and if not, enters it into artist
+#returns id.
+def artist_entry(db, artist_name) #
   artists = db.execute("SELECT * FROM artist")
   exsists = false
   id = nil
@@ -54,6 +61,8 @@ def artist_entry(db, artist_name)
   id
 end
 
+#Checks if entry exists and if not, enters it into label
+#returns id.
 def label_entry(db, label_name)
   labels = db.execute("SELECT * FROM label")
   exsists = false
@@ -69,7 +78,6 @@ def label_entry(db, label_name)
     labels = db.execute("SELECT * FROM label")
       labels.each do |row|
         if row['label'] == label_name
-        exsists = true
         id = row['id']
         end
       end
@@ -85,5 +93,25 @@ def album_entry(db, artist, title, label, rating, release_date)
   )
 end
 
-album_entry(db, artist_entry(db,"Yet Another"), "Something", label_entry(db, "New Label"), 1, 1997)
+#album_entry(db, artist_entry(db,"Yet Another"), "Something", label_entry(db, "New Label"), 1, 1997)
 
+answer = "y"
+puts "Lets enter some vinyl info!"
+
+until answer == "n"
+  puts "What's the artist's name?"
+  artist = gets.chomp
+  puts "What's the title of the album?"
+  title = gets.chomp
+  puts "what's the label that published it?"
+  label = gets.chomp
+  puts "What would you give it out of 5 stars?"
+  rating = gets.chomp.to_i
+  puts "What year did it come out?"
+  release_date = gets.chomp.to_i
+  album_entry(db, artist_entry(db, artist), title, label_entry(db, label), rating , release_date)
+  puts "Would you like to view your records?"
+  records = gets.chomp[0].downcase
+  puts "Would you like to enter another record? y/n"
+  answer = gets.chomp[0].downcase
+end
